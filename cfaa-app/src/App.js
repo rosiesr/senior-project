c197c98965cd62e2846da3b5e2309d2c55e42af7
Rosie Rothschild <rosierothschild@Rosies-MBP.stdusr.yale.internal>
2024-02-27 15:49:44 -0500

    created react app for frontend code

---
+++ cfaa-app/src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Computer Fraud and Abuse Act SMT Solver
+        </p>
+        <button>
+          Try it!
+        </button>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
